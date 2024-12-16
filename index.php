d57ea235f4e8a69e490800b611bd2f64bf45eee8
umniknik <kaaaaaljan@mail.ru>
2024-12-16 19:03:13 +0300

    ДЗ к занятию «1.3. Базовые операторы и функции» Задание 2: "Стандартизатор имени"

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+  <title>PHP Test</title>
+</head>
+
+<body>
+
+
+  <?php
+
+  $firstName = readline('ВВедите имя: ');
+  $secondName = readline('Введите фамилию: ');
+  $fatherName = readline(prompt: 'Введите отчество: ');
+
+  // преобразует первый символ строки в верхний регистр, используем встроенную функцию mb_ucfirst.
+  $firstName = mb_ucfirst($firstName, "utf-8");
+  $secondName = mb_ucfirst($secondName, "utf-8");
+  $fatherName = mb_ucfirst($fatherName, "utf-8");
+
+  $fullname = $secondName . ' ' . $firstName . ' ' . $fatherName;
+
+  echo "Полное имя: '$fullname' \n";
+
+  //С помощью функции mb_substr достанем первую букву из каждой переменной
+  $firstLetterFirstName = mb_substr($firstName, 0, 1);
+  $firstLetterFatherName = mb_substr($fatherName, 0, 1);
+  $firstLetterSecondName = mb_substr($secondName, 0, 1);
+
+  $surnameAndInitials = $secondName . ' ' . $firstLetterFirstName . '.' . $firstLetterFatherName;
+
+  echo "Фамилия и инициалы: '$surnameAndInitials' \n";
+
+  $fio = "'$firstLetterSecondName.$firstLetterFirstName.$firstLetterFatherName'";
+
+  echo "Аббревиатура: $fio";
+
+  ?>
+
+</body>
+
+
+</html>
